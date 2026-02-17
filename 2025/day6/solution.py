03770cd034ca67f0b67560fa50b16e4351c24ae9
bhalliga <bhalliga@med.umich.edu>
2026-02-17 00:51:12 -0500

    2025 day 6 part 2 needs parsing fix

--- 2025/day6/solution.py
+++ 2025/day6/solution.py
@@ -21,14 +21,51 @@ def part1(file):
         count += calc(a,b,c,d,oper)
     return count
 
+def find_greatest(a,b,c,d):
+    return max([a,b,c,d], key=len)
+def get_nums(a,b,c,d):
+    # flip number order
+    print(len(a), len(b), len(c), len(d))
+    first = a[3]+b[3]+c[3]+d[3]
+    second = a[2]+b[2]+c[2]+d[2]
+    thrid = a[1]+b[1]+c[1]+d[1]
+
+    return (int(first), int(second), int(thrid))
+
 def part2(file):
     with open(file) as f:
         lines = f.read().splitlines()
-    lines = [l.split() for l in lines]
-    print(lines)
+    indices = len(lines[0]) # number of problems
+    print(indices)
+    numbers = []
+
+    # get number groups
+    test = 0
+    prev = 0
+    for i in range(5, indices+6, 5):
+        a = lines[0][prev:i][:3]
+        b = lines[1][prev:i][:3]
+        c = lines[2][prev:i][:3]
+        d = lines[3][prev:i][:3]
+        print(a,b,c,d)
+        # numbers.append(get_nums(a,b,c,d))
+        prev = i
+        test+=1
+        if test > 3:
+            break
+
+    opers = lines[4].split()
+    count = 0
+    for n, o in zip(numbers,opers):
+        if o == '*':
+            count+= (n[0]*n[1]*n[2]*n[3])    
+        elif o == '+':
+            count+= (n[0]+n[1]+n[2]+n[3])
+    return count
+
 
 if __name__ == "__main__":
     path = 'input.txt'
     test = 'test.txt'
     print("Part 1:", part1(path))
-    print('Part 2:', part2(test))
+    print('Part 2:', part2(path))
